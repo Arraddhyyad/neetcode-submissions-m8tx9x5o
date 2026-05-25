0f56c4061046a3b260e3d4151c227e18e7c71780
Aradhya Dixit <154388171+Arraddhyyad@users.noreply.github.com>
2026-05-25 09:22:39 -0700

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            // i >> 1 is the same as i / 2
+            // i & 1 is 1 if odd, 0 if even
+            res[i] = res[i >> 1] + (i & 1); 
+        }
+        
+        return res;
+    }
+}
